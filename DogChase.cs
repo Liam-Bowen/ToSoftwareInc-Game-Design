c8e47f92d14df86d983c7831dd2ab925dd2b01e5
Lukas Howlett <112579284+Lukas-Howlett@users.noreply.github.com>
2023-04-23 22:09:09 -0700

    Updated the DogChase script
    Now Includes animations for dog movement

--- DogChase.cs
+++ DogChase.cs
@@ -5,30 +5,41 @@ using System.Collections;
 
  public class DogChase : MonoBehaviour
  {
+    Rigidbody rb;
     public Transform Player;
     public Transform guardSpot;
     public Transform target;
+    Animator animator;
     float distanceFromPlayer;
     int MoveSpeed = 4;
     int MinDist = 3;
 
     void Awake(){
         target = guardSpot;
+        animator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody>();
     }
 
     void Update()
     {
+        animator.SetFloat("Velocity", rb.velocity.magnitude);
         distanceFromPlayer = Vector3.Distance(transform.position, Player.position);
         transform.LookAt(target);
+        //print(rb.velocity.magnitude);
  
         if (distanceFromPlayer < 10.0)
         {
+            animator.SetBool("TargetingPlayer", true);
+            animator.SetBool("Awake", true);
             target = Player;
             transform.position += transform.forward * MoveSpeed * Time.deltaTime;
         }else{
+            animator.SetBool("TargetingPlayer", false);
             target = guardSpot;
             if(Vector3.Distance(transform.position, guardSpot.position) >= MinDist){
                 transform.position += transform.forward * MoveSpeed * Time.deltaTime;
+            }else{
+                animator.SetBool("Awake", false);
             }
         }
     }
